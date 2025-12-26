3fd35eb87cbd9a131399cc78b8be3537810890cc
William Glory <williamgloryh@gmail.com>
2025-12-26 14:24:06 +0700

    feat: change hardhat config

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,8 +1,21 @@
-// TODO: Configure Hardhat for the private PoA network and project settings.
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+
   networks: {
-    // Define your custom network RPC URL and accounts here.
+    // Local Hardhat node (buat compile/deploy + FE dev)
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+
+    // Private PoA network (isi nanti kalau RPC sudah ada)
+    // poa: {
+    //   url: process.env.POA_RPC_URL,
+    //   accounts: process.env.DEPLOYER_PK ? [process.env.DEPLOYER_PK] : [],
+    //   chainId: Number(process.env.POA_CHAIN_ID || "0"),
+    // },
   },
 };
